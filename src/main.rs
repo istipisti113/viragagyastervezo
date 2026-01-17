9040de03cd42a5b6f7ece9cde43fe2a9590a5c09
Szabo Istvan <pisti4395@gmail.com>
2026-01-17 10:28:16 +0100

    sadf

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use warp::{filters::path::param, reply::{Reply, Response}, Filter};
+use std::fs;
+use regex::Regex;
+
+#[tokio::main]
+async fn main() {
+    let port = 4040;
+    println!("running on {}", port);
+    let home = warp::path::end().map(|| warp::reply::html(fs::read_to_string("html/index.html").unwrap()));
+
+    let routes = home;
+    warp::serve(routes).run(([0,0,0,0], port)).await;
 }
