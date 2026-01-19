4dbd54d66f9152aa4031d64a76e109223b9e24fa
Szabó István <pisti4395@gmail.com>
2026-01-19 07:58:54 +0100

    noveny json fetch

---
+++ script.js
@@ -0,0 +1,15 @@
+function loadplants(){
+    fetch("novenyek")
+}
+async function loadPage(page, id) {
+  return fetch(page)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
+    })
+}
+
+async function loadJson(page) {
+  return fetch(page)
+    .then(response => response.text())
+}
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use dotenvy::dotenv;
 mod request;
 use request::RequestBuilder;
 
-#[derive(serde::Deserialize, Debug)]
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
 struct noveny{
     id: i16,
     neve: String,
@@ -21,16 +21,21 @@ async fn main() {
     let port = 4040;
     println!("running on {}", port);
 
+    //let novenyek: Vec<noveny> = RequestBuilder::new().url("https://qrugmxvevfhnipzirkdy.supabase.co/rest/v1").table("faj").select("*").run_struct().unwrap();
+
     //path
     let home = warp::path::end().map(|| warp::reply::html(fs::read_to_string("html/index.html").unwrap()));
     let style = warp::path!("style.css").and(warp::fs::file("html/style.css"));
+    let script = warp::path!("script.js").and(warp::fs::file("script.js"));
     let background = warp::path!("background.jpg").and(warp::fs::file("images/background.jpg"));
+    let novenyek = warp::path!("novenyek").map(||{
+        let plants: Vec<noveny> = RequestBuilder::new().url("https://qrugmxvevfhnipzirkdy.supabase.co/rest/v1").table("faj").select("*").run_struct().unwrap();
+        warp::reply::json(&serde_json::to_string(&plants).unwrap())
+    });
 
-    let novenyek: Vec<noveny> = RequestBuilder::new().url("https://qrugmxvevfhnipzirkdy.supabase.co/rest/v1")
-        .table("faj").select("*").run_struct().unwrap();
 
     println!("{}", RequestBuilder::new().table("faj").select("id").run_str().unwrap().join(" "));
-    let routes = home.or(style).or(background);
+    let routes = home.or(style).or(background).or(novenyek).or(script);
     warp::serve(routes).run(([0,0,0,0], port)).await;
 }
 
