d866a262273eab683bdfea6f3034bafc17ff9d79
Szabó István <pisti4395@gmail.com>
2026-01-19 11:08:28 +0100

    legordulo menu fasza

--- html/asd.js
+++ html/asd.js
@@ -2,11 +2,10 @@
 document.getElementById('open_popup').addEventListener('click', async function() {
     document.getElementById('popup_overlay').classList.add('active');
     var lista = await loadJson("query/faj/neve")
-    var sor = "<option value='NEVE'>NEVE</option>"
+    var sor = "<option value='NEVE' onclick='handleSelectClick(event)'>NEVE</option>"
     var asdf = ""
     for (let name in lista) {
         asdf+=sor.replaceAll("NEVE", lista[name])
-        console.log(lista[name])
     }
     document.getElementById("plant_select").innerHTML = asdf
 });
@@ -40,3 +39,15 @@ document.addEventListener('keydown', function(e) {
         document.getElementById('popup_overlay').classList.remove('active');
     }
 });
+
+async function handleSelectClick(event) {
+    const select = event.target.value;
+    var id = await loadJson("get_by/faj/neve/"+select+"/id") 
+    var fajtak = await loadJson("get_by/fajta/fajid/"+id+"/neve") 
+    var sor = "<option value='NEVE'>NEVE</option>"
+    var asdf = ""
+    for (let name in fajtak) {
+        asdf+=sor.replaceAll("NEVE", fajtak[name])
+    }
+    document.getElementById("fajta_select").innerHTML = asdf
+}
