951ec6e10712a4d9631825a762cef7b4b9e00699
Szabó István <pisti4395@gmail.com>
2026-02-02 07:57:57 +0100

    html canvas

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ async function loadJson(page) {
   return fetch(page)
     .then(response => response.json()).then(final => JSON.parse(final))
 }
+var displayWidth = 980;
+var displayHeight = 810;
+var canvas = document.getElementById("c");
+var scale = 2;
+canvas.style.width = displayWidth + 'px';
+canvas.style.height = displayHeight + 'px';
+canvas.width = displayWidth * scale;
+canvas.height = displayHeight * scale;
+
+var c = document.getElementById("c");
+var ctx = c.getContext("2d");
+
+ctx.moveTo(0, 0);
+ctx.lineTo(980*2, 810*2);
+ctx.stroke();
+
