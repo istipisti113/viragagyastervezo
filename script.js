e31aec158a5fd3b4e31e9b58955909da1cf140c8
Szabo Istvan <pisti4395@gmail.com>
2026-02-09 13:29:40 +0100

    tervezes

--- script.js
+++ script.js
@@ -144,15 +144,14 @@ function hozzaadas() {
 
 var displayWidth = 980;
 var displayHeight = 810;
-var canvas = document.getElementById("c");
+var canvas = document.getElementById("gardenCanvas");
 var scale = 2;
 canvas.style.width = displayWidth + 'px';
 canvas.style.height = displayHeight + 'px';
 canvas.width = displayWidth * scale;
 canvas.height = displayHeight * scale;
 
-var c = document.getElementById("c");
-var ctx = c.getContext("2d");
+var ctx = canvas.getContext("2d");
 
 ctx.moveTo(0, 0);
 ctx.lineTo(980*2, 810*2);
